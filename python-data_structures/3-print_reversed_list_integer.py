2d555f434cafdda7019e342eb9e8bfff5e25d5d7
Erkenoss <xavier.bertin6@gmail.com>
2023-10-02 15:46:26 +0200

    print if my_list = null

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) is None:
-        return
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    if len(my_list) != None:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
