2ac60855318fb3cebc19b4537e9ade738a8258e9
Erkenoss <xavier.bertin6@gmail.com>
2023-10-02 16:31:23 +0200

    fixed task 5-

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    result = ""
     if my_string is not None:
         lenght = len(my_string)
         for i in range(lenght):
             if my_string[i] == 'C' or my_string[i] == 'c':
                 continue
-            print("{}".format(my_string[i]), end="")
+            result += my_string[i]
+    return result
