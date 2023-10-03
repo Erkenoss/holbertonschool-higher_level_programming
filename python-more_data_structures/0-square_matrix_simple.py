a43a8e30ac9a2db52dc9a72cf9e1218d527f691a
Erkenoss <xavier.bertin6@gmail.com>
2023-10-03 14:30:30 +0200

    fixed task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is not None:
-        new_matrix = []
-        for i in new_matrix:
-            new_row = []
-            for j in i:
-                new_row.append(j ** 2)
-            new_matrix.append(new_row)
-        return new_matrix
-    else:
-        return None
+
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for ligne in row:
+            new_row.append(ligne ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
