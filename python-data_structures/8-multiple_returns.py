d5de58346d896d2266c3ef325bad0063be631088
Erkenoss <xavier.bertin6@gmail.com>
2023-10-03 10:22:59 +0200

    add task 8-

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
 
-    if sentence is not None:
-        temp_t = (len(sentence), sentence[0])
-    else:
+    if len(sentence) == 0:
         temp_t = (0, None)
+    else:
+        temp_t = (len(sentence), sentence[0])
 
     return temp_t
