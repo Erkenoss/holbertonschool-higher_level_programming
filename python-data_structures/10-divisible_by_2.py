3f45253073f5a6b197078395eff55d26e1b1687c
Erkenoss <xavier.bertin6@gmail.com>
2023-10-03 10:59:50 +0200

    pycode task  10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
 
-    if my_list != None or len(my_list) != 0:
+    if my_list is not None or len(my_list) != 0:
         new_list = []
         for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
