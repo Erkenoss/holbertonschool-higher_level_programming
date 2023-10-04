4067c34473b74f3465098a7f49df1415b3a564db
Erkenoss <xavier.bertin6@gmail.com>
2023-10-04 19:04:12 +0200

    synthaxe on internet

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not None and roman_string is not "":
+    if roman_string is not None:
         dct = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         result = 0
         prev_value = 0
@@ -12,5 +12,3 @@ def roman_to_int(roman_string):
                 result += value
             prev_value = value
         return result
-    else:
-        return None
