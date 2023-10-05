27e4a6996fa8dde13ee31ce61e263341ee575963
Erkenoss <xavier.bertin6@gmail.com>
2023-10-05 10:49:21 +0200

    fixed task 12-

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not None:
-        dct = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        result = 0
-        prev_value = 0
-        for char in reversed(roman_string):
-            value = dct[char]
-            if value < prev_value:
-                result -= value
-            else:
-                result += value
-            prev_value = value
-        return result
+
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+
+    dct = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        value = dct[char]
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+    return result
