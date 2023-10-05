c9ce0a9686d09b0ac55e6f9782f56cdcdf3f60dc
Erkenoss <xavier.bertin6@gmail.com>
2023-10-05 12:36:01 +0200

    add task 3-

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a/b
+        print("Inside result: {}".format(result))
+    except ZeroDivisionError:
+        result = "None"
+        print("Inside result: {}".format(result))
+    except (ValueError):
+        pass
+    finally:
+        return (result)
