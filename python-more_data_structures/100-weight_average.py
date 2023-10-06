1a76056cc351d77e53f47e041d07836a0f9d43b3
Erkenoss <xavier.bertin6@gmail.com>
2023-10-06 15:35:23 +0200

    add first advanced

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+
+    sum_weight = 0
+    total_weight = 0
+
+    for tup in my_list:
+        value = tup[0]
+        weight = tup[1]
+
+        sum_weight += value * weight
+        total_weight += weight
+
+    if weight != 0:
+        average = sum_weight / total_weight
+        return average
+    else:
+        return 0
