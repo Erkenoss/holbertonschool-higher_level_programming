86ff8621a4ab1ddeb2b03fceae280bc2a4694696
Erkenoss <xavier.bertin6@gmail.com>
2023-10-09 11:53:04 +0200

    add task 3-

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Update class Square and add private instance attribute size"""
+
+
+class Square:
+
+    """Add exception in the class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
