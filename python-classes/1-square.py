fa1a4d4682b2f0b9c789a22216948cb4dfc27b30
Erkenoss <xavier.bertin6@gmail.com>
2023-10-09 10:22:19 +0200

    add task 1-

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Update class Square and add private instance attribute size"""
+
+
+class Square:
+
+    """Add private attribute size"""
+    def __init__(self, size):
+        self.__size = None
