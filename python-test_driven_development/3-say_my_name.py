fcb7832c4b72af7360e35b7ca4b3366f015b1e60
Erkenoss <xavier.bertin6@gmail.com>
2023-10-12 11:50:02 +0200

    update test cases

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""function for print name and last name"""
 
 
 def say_my_name(first_name, last_name=""):
-    """function for print name and last name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
