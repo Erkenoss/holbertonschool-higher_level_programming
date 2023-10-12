a1609daee6e3a02d7a393cc1faca9254a049cc4b
Erkenoss <xavier.bertin6@gmail.com>
2023-10-12 10:28:37 +0200

    changes test all row

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,12 +15,15 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for row in matrix:
-        num_col = len(matrix[0])
-        for col in row:
-            if len(row) != num_col:
-                raise TypeError("Each row of the matrix must\
-                    have the same size")
+    # for row in matrix:
+    #     num_col = len(matrix[0])
+    #     for col in row:
+    #         if len(row) != num_col:
+    #             raise TypeError("Each row of the matrix must\
+    #                 have the same size")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must \
+            have the same size")
 
     new_matrix = [[round(col / div, 2) for col in row] for row in matrix]
     return new_matrix
