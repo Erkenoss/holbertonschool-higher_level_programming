576ecf043bdc7bc546594bb8e8893466096f3489
Erkenoss <xavier.bertin6@gmail.com>
2023-10-13 10:23:49 +0200

    fixed casse size 0

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -12,8 +12,5 @@ def print_square(size):
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
-    if size == 0:
-        print()
-    else:
-        for i in range(size):
-            print("#" * size)
+    for i in range(size):
+        print("#" * size)
