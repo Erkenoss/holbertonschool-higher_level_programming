021d5ebbf6b44f7c459d706ca7fc57342b329b6f
Erkenoss <xavier.bertin6@gmail.com>
2023-10-17 13:17:40 +0200

    pycode

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,6 @@
 
 
 def inherits_from(obj, a_class):
-    """return True if obj is an insstance of a_class ddirectly or not,from the speecific class"""
+    """return True if obj is an insstance of a_class ddirectly
+    or not,from the speecific class"""
     return isinstance(obj, a_class) and type(obj) != a_class
