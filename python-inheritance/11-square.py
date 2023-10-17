20d7ecb521b80f973a7a4a0b978bd3d0579940bc
Erkenoss <xavier.bertin6@gmail.com>
2023-10-17 17:18:20 +0200

    add task 11-

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""start of class Square"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """create a square with class Rectangle"""
+    def __init__(self, size):
+        """define a square base on Rectangle"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """return size * size for have an area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
