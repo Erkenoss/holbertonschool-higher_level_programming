da3f44be872c05142cece2dbd32d481c46ae8afe
Erkenoss <xavier.bertin6@gmail.com>
2023-10-17 13:30:06 +0200

    add task 6-

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Start of class base_geometry"""
+
+
+class BaseGeometry:
+    """Create a object"""
+    def area(self):
+        """Define the area of an object"""
+        raise Exception("area() is not implemented")
