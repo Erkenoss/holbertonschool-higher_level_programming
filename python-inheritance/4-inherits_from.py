5ee8e8789323d7a46c2d8d3cc451ce41ddf599ec
Erkenoss <xavier.bertin6@gmail.com>
2023-10-17 13:14:57 +0200

    add task 4-

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Start of a function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """return True if obj is an insstance of a_class ddirectly or not,from the speecific class"""
+    return isinstance(obj, a_class) and type(obj) != a_class
