f59a544b7e2e38f82bce877eb4bba5e0ef45c878
Erkenoss <xavier.bertin6@gmail.com>
2023-10-17 12:40:26 +0200

    add task 3-

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Start of function is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return if obj is in a_class of not"""
+    return isinstance(obj, a_class)
