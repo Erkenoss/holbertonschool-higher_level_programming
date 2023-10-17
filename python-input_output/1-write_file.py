df57c95b9be2c37ecfefcd545d5eebb9a7e0ff97
Erkenoss <xavier.bertin6@gmail.com>
2023-10-17 18:03:22 +0200

    add task 1-

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """write text in a new file"""
     with open(filename, 'w', encoding="utf-8") as f:
         write_data = f.write(text)
         return write_data
