b1eab15133aeefb76a06d82e5103c1b1c2fd82ac
Erkenoss <xavier.bertin6@gmail.com>
2023-10-18 13:22:12 +0200

    fixed last check task 7-

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Start of function"""
 import sys
-
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
+
+if os.path.exists(filename) and os.path.getsize(filename) > 0:
+    my_list = load_from_json_file(filename)
+else:
     my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+
+my_list.extend(args[1:])
+save_to_json_file(my_list, filename)
