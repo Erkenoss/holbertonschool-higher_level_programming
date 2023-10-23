d8ebbed9ee0bfcc38d2b32fe408cdfb64c9227c5
Erkenoss <xavier.bertin6@gmail.com>
2023-10-23 17:27:49 +0200

    add task 10-

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """start class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be > 0")
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} \
+- {self.width}"
