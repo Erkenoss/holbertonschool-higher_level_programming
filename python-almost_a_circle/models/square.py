fdd26db5f8add93fc0c06034bc929a6e151332da
Erkenoss <xavier.bertin6@gmail.com>
2023-10-23 18:56:26 +0200

    add task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -34,3 +34,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        new_dict = {
+            'id' : self.id,
+            'size' : self.width,
+            'x' : self.x
+            'y' : self.y
+        }
+        return new_dict
