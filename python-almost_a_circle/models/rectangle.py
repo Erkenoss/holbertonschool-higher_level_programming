cbf30e33289f0b6adabb0ddb1b5d8b7370aa7c68
Erkenoss <xavier.bertin6@gmail.com>
2023-10-23 13:31:24 +0200

    update task 3-

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """start of class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Init of Rectangle"""
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width <= 0:
