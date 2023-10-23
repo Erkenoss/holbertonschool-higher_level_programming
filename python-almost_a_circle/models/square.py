12970b41cd9e846a4fdb5ec0593b25a12db87d35
Erkenoss <xavier.bertin6@gmail.com>
2023-10-23 17:53:37 +0200

    update square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,18 +6,6 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """start class Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size <= 0:
-            raise ValueError("size must be > 0")
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
