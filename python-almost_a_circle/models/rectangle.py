6f8157a99a9066c1341b20858ccd77da3bc29116
Erkenoss <xavier.bertin6@gmail.com>
2023-10-23 14:34:43 +0200

    add task 7-

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -159,5 +159,5 @@ class Rectangle(Base):
             print(" " * self.x + "#" * self.__width)
 
     def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
-
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} \
+- {self.__width}/{self.__height}"
