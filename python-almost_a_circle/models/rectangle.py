91a16ee2a6c6ab037d51ca16ea79b185784d049c
Erkenoss <xavier.bertin6@gmail.com>
2023-10-23 13:39:35 +0200

    add task 3-

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """start of class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Init of Rectangle"""
+
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -31,10 +31,26 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not within a valid range.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -43,10 +59,26 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not within a valid range.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -55,24 +87,56 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """
+        Get the x-coordinate of the top-left corner.
+
+        Returns:
+            int: The x-coordinate.
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        Set the x-coordinate of the top-left corner.
+
+        Args:
+            value (int): The new x-coordinate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not within a valid range.
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be > 0")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """
+        Get the y-coordinate of the top-left corner.
+
+        Returns:
+            int: The y-coordinate.
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        Set the y-coordinate of the top-left corner.
+
+        Args:
+            value (int): The new y-coordinate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not within a valid range.
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value <= 0:
-            raise ValueError("y must be > 0")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
