1fe5c701b1fc170c303255c630bbd000299d0f64
Erkenoss <xavier.bertin6@gmail.com>
2023-11-20 16:18:50 +0100

    add task 9-

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
--- create a full ssecond_table in a database
+-- create a full second_table in a database
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
-)
+);
 
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
