1dd42612b03298657f3ad448f4ed3a631154d37a
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 15:45:40 +0100

    add task 16-

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title , CASE WHEN tv_shows IS NOT NULL THEN tv_shows.title ELSE 'NULL' END
+tv_genres.name
 FROM tv_show_genres, tv_shows
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
