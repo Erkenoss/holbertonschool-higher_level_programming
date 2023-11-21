c73ee51b61d3d5827cf59f1b83dfb751e241f4eb
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 11:17:42 +0100

    add task 7-

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- create table and database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
