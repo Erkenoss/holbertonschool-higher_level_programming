596930d6615a9eb6a6b335fc5a28bf9fba533b0d
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 11:58:41 +0100

    fixed fromm

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list all the citie in a table
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROMM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id;
