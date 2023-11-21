5043115234dffac8279ba74feee458241bee5971
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 10:54:58 +0100

    add USE

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-hbtn_0d_usa;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT NOT NULL,
