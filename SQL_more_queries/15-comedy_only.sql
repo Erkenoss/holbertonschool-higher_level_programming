7142271b313c2e6c01552a68eb2433edaf277e5c
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 15:38:32 +0100

    fixed?

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_genres.name
+SELECT tv_shows.title
 FROM tv_show_genres
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
