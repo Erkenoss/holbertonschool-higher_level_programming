13d537b061c023429cf38cd0ed587ce38fcec300
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 14:38:25 +0100

    add LEFT to JOIN

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
 --  a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_show_genres.genre AS genre, COUNT(tv_shows.id) AS number_of_shows
+SELECT tv_show_genres.genre AS genre,
+COUNT(tv_shows.id) AS number_of_shows
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 GROUP BY tv_show_genres.genre
 WHERE COUNT(number_of_shows) > 0
 ORDER BY number_of_shows DESC;
