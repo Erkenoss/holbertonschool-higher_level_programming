80c92f75c9b32e04636968a478a2bf5b4052912c
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 15:48:49 +0100

    fixed LEFT

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT tv_shows.title , CASE WHEN tv_shows IS NOT NULL THEN tv_shows.title ELSE 'NULL' END
-tv_genres.name
-FROM tv_show_genres, tv_shows
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL')
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name;
