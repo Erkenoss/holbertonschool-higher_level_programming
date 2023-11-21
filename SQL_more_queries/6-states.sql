ae0816bd657cf358ff825031ddabbdb01fde1980
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 10:53:42 +0100

    add task 6-

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- create database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (id)
+);
