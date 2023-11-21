3e8b5bd2442295e97e451274653f1fe98b1df35a
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 13:13:32 +0100

    change indent

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, CASE WHEN tv_show_genres.genre_id IS NOT NULL THEN tv_show_genres.genre_id ELSE 'NULL' END AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
