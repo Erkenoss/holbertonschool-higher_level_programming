b412d73303dbe01d90eaf714c654a5e13084e993
Erkenoss <xavier.bertin6@gmail.com>
2023-11-21 12:08:21 +0100

    add task 9-

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- list all cities in database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name AS states
-FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
