abb148b28aecc3301122946d04960ab3d0b1a79c
Erkenoss <xavier.bertin6@gmail.com>
2023-11-26 20:44:04 +0100

    try to change something for pass the requirement

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,18 +7,19 @@ import sys
 
 
 if __name__ == "__main__":
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3]
-        )
+    )
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
 
-    for row in cur.fetchall()
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
