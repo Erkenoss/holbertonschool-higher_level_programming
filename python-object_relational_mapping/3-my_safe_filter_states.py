cbd656be2d80f7fdbf8837ea5a66c46b29f8c178
Erkenoss <xavier.bertin6@gmail.com>
2023-11-26 21:53:45 +0100

    pycode

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument with safe against
+in the states table of hbtn_0e_0_usa where
+name matches the argument with safe against
 SQL injection
 """
 import MySQLdb
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    
+
     query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id"
     name_to_search = sys.argv[4]
     cursor.execute(query, (name_to_search,))
