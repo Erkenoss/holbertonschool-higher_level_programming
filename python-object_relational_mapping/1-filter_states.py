21f0a6fd8832babe9216554c7323b00e2bcb0e96
Erkenoss <xavier.bertin6@gmail.com>
2023-11-26 21:36:25 +0100

    fixed pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -17,7 +18,8 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    cursor.execute("SELECT * FROM states WHERE name
+                   LIKE BINARY ORDER BY states.id 'N%'")
 
     for row in cursor.fetchall():
         print(row)
