7eacdb71bf91d057aa06da531f6b12246401dc0b
Erkenoss <xavier.bertin6@gmail.com>
2023-11-27 13:46:46 +0100

    fixed?

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,14 +13,15 @@ from sys import argv
 if __name__ == "__main__":
     username, password, database = argv[1], argv[2], argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database)
+                           .format(username, password, database),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     result = (
         session.query(City, State)
-        .filter(State, City.state_id == State.id)
+        .filter(City.state_id == State.id)
         .order_by(City.id)
         .all()
     )
