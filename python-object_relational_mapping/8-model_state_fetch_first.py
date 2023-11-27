47078c3a5c3599a059e9c548a7d91c62224ba1a6
Erkenoss <xavier.bertin6@gmail.com>
2023-11-27 11:48:06 +0100

    add task 8-

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    username, password, database = argv[1], argv[2], argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    states = session.query(State).order_by(State.id).first()
+
+    if states is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(states.id, states.name))
+
+    session.close()
