04d58377d470ebee676502e45490b533805fdebd
Erkenoss <xavier.bertin6@gmail.com>
2024-01-29 14:57:46 +0100

    add task 4-

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const addItemElement = document.getElementById('add_item');
+  const myListElement = document.querySelector('.my_list');
+
+  addItemElement.addEventListener('click', function () {
+    const newListItem = document.createElement('li');
+    newListItem.textContent = 'Item';
+
+    myListElement.appendChild(newListItem);
+  });
+});
